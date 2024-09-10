9d355e548e8650336f969050f3e609c86b839c74
Gjermund Garaba <gjermund@garaba.net>
2024-09-10 10:30:42 +0200

    update docs baseUrl after moving to custom domain

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://interchain-attestation.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/interchain-attestation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
